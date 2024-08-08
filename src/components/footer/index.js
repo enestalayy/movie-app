0412dc02e827bd5f3419e3e74a8fc9a7eff79402
Enes Talay <enestalayy@gmail.com>
2024-08-08 20:22:25 +0300

    first commit

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+import styles from "./styles.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      Made with ❤️ by&nbsp; Yigit Cakmak
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/skeleton/index.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+const Skeleton = ({ width, height }) => {
+  return (
+    <div className={styles.skeleton} style={{ width, height }}>
+      Skeleton
+    </div>
+  );
+};
+
+export default Skeleton;
